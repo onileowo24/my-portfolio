f4835aa04599c907993e07c953392fc61ed93336
onileowo24 <rhodestara14@gmail.com>
2023-06-13 08:05:50 +0100

    first update

---
+++ script.js
@@ -0,0 +1,23 @@
+var typingText = document.querySelector('.text2')
+var myArray =
+    ["Web Developer", "Geographer", "Freelancer", "Graphic Designer", "Affiliate Marketing"];
+var arrayIndex = 1;
+
+function textReplace() {
+    setInterval(timer, 5000);
+
+    function timer() {
+        if (arrayIndex < myArray.length) {
+            typingText.innerHTML = myArray[arrayIndex];
+            arrayIndex = arrayIndex + 1;
+        }
+        else {
+            arrayIndex = 0;
+            typingText.innerHTML = myArray[arrayIndex];
+            arrayIndex = arrayIndex + 1;
+        }
+    }
+
+    console.log(typingText)
+}
+textReplace();
